c886ae32af671765dcbc0ef0dc4b6c90593b024b
doinki <doinki94@gmail.com>
2021-11-20 05:13:17 +0900

    🎉

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  
+  html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  *::after,
+  *::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: Roboto, 'Noto Sans KR', sans-serif;
+  }
+
+  #__next {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export default GlobalStyle;
