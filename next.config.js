2110f7b21c5b45201b5059273e353c450b4b7741
doinki <doinki94@gmail.com>
2021-12-23 22:18:44 +0900

    🚀

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  // experimental: {
-  //   scrollRestoration: true,
-  //   styledComponents: true, // https://nextjs.org/docs/advanced-features/compiler#styled-components
-  // },
+  experimental: {
+    // scrollRestoration: true,
+    styledComponents: true, // https://nextjs.org/docs/advanced-features/compiler#styled-components
+  },
   poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true,
 };
