a29e3ddf66051101e08e0c047c4f85a2588ba56f
doinki <doinki94@gmail.com>
2022-03-13 01:36:46 +0900

    🚀

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['next-seo']);
 
 /**
  * @type {import('next').NextConfig}
@@ -16,4 +18,4 @@ const nextConfig = {
   swcMinify: true,
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withPlugins([withBundleAnalyzer, withTM], nextConfig);
